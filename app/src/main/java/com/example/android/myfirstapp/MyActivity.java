54bc2b90c6de20decb6373f8e94ef9fb143497a0
Pablo Munoz <pfariasmunoz@gmail.com>
2016-03-15 19:27:10 -0300

    Starting another activity

--- app/src/main/java/com/example/android/myfirstapp/MyActivity.java
+++ app/src/main/java/com/example/android/myfirstapp/MyActivity.java
@@ -1,15 +1,21 @@
 package com.example.android.myfirstapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Display;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
 
 public class MyActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.example.android.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +54,14 @@ public class MyActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
